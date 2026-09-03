a073482097a232b2a85afebe75c8d609c997fabe
azdine06 <oazdine06@gmail.com>
2026-09-03 17:46:23 +0100

    Refactor code structure for improved readability and maintainability

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,9 +2,8 @@ import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-  earlyAccess: true,
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
   },
-});
+} as any);
